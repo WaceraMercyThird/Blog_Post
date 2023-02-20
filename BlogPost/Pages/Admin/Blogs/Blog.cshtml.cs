45c063f7494be3204ed11940e79e76cfc403e93d
Mercy Wacera <waceraaamercy@gmail.com>
2023-02-20 15:55:42 +0300

    Added UI

---
+++ BlogPost/Pages/Admin/Blogs/Blog.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogPost.Pages.Admin.Blogs
+{
+	public class BlogModel : PageModel
+    {
+       
+        public void OnGet()
+        {
+
+        }
+
+        public void onPost()
+        {
+            var heading = Request.Form["heading"];
+            var pageTitle = Request.Form["pageTitle"];
+            var shortDescription = Request.Form["shortDescription"];
+            var featuredImageUrl = Request.Form["featuredImageUrl"];
+        }
+    }
+}
